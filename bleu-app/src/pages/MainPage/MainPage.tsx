2daaaaf65c22b8cb8e947a640af0577d2fb2a5d7
Jeeyong Um <conr2d@gmail.com>
2021-09-21 20:20:08 +0900

    Test popup menu

--- bleu-app/src/pages/MainPage/MainPage.tsx
+++ bleu-app/src/pages/MainPage/MainPage.tsx
@@ -5,8 +5,10 @@ import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import LatestBlocks from './components/LatestBlocks';
 import LatestTransactions from './components/LatestTransactions';
+import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import Overview from './components/Overview';
+import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 
 const root: Readonly<any> = {
@@ -52,7 +54,22 @@ const band: Readonly<any> = {
   position: 'absolute',
 };
 
+const popup = {
+  padding: '10px',
+};
+
 function MainPage() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+  const handleMouseEnter = (event: any) => {
+    setAnchorEl(event.currentTarget);
+    console.log('mouse enter');
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    console.log('mouse leave');
+  };
   return (
     <Box sx={root}>
       <Box sx={main}>
@@ -62,7 +79,16 @@ function MainPage() {
           </Typography>
           <Box sx={{display:{xs:'none',sm:'none',md:'block'}}}>
             <Button sx={menu}>Home</Button>
-            <Button sx={menu} href='/blocks'>Blockchain</Button> <Button sx={menu}>Tokens</Button>
+            <Button sx={menu} aria-describedby={id} onMouseEnter={handleMouseEnter}>Blockchain</Button>
+            <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}>
+              <Box sx={popup} onMouseLeave={handleClose}>
+                <Link href='/blocks'><Typography>Blocks</Typography></Link>
+              </Box>
+            </Popover>
+            <Button sx={menu}>Tokens</Button>
             <Button sx={menu}>Resources</Button>
             <Button sx={menu}>Misc</Button>
           </Box>
