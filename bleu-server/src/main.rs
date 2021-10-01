06fdb25d6a95d6a6f13a17082be2fde236da1a93
jungyongum <ian.jungyong.um@gmail.com>
2021-10-01 18:38:48 +0900

    add dotenv

--- bleu-server/src/main.rs
+++ bleu-server/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate diesel;
 
+use std::env;
+
 use actix_web::{App, HttpServer, web};
 use diesel::{PgConnection, r2d2};
 use diesel::r2d2::ConnectionManager;
@@ -17,17 +19,24 @@ type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let manager = ConnectionManager::<PgConnection>::new("postgres://root:postgresql@localhost:5432/postgres");
+    dotenv::dotenv().ok();
+
+    let postgres_url = env::var("POSTGRES_URL").expect("POSTGRES_URL does not exist!");
+    let manager = ConnectionManager::<PgConnection>::new(postgres_url);
     let pool = r2d2::Pool::builder().build(manager).unwrap();
+
+    let host = env::var("SERVER_HOST").expect("SERVER_HOST does not exist!");
+    let port = env::var("SERVER_PORT").expect("SERVER_PORT does not exist!");
+    let endpoint = format!("{host}:{port}", host = host, port = port);
 
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
             .service(web::scope("/api/v1")
-                .route("/ethereum/block/{eth_blocks_id}", web::get().to(get_eth_block_by_id))
+                .service(get_eth_block_by_id)
             )
     })
-        .bind("127.0.0.1:8080")?
+        .bind(endpoint)?
         .run()
         .await
 }
