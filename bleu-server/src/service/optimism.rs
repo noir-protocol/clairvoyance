c0039433eddc876d55191c55c4ad1bc67158530a
jungyongum <ian.jungyong.um@gmail.com>
2021-10-15 17:25:32 +0900

    add searching state batch apis

--- bleu-server/src/service/optimism.rs
+++ bleu-server/src/service/optimism.rs
@@ -5,20 +5,20 @@ use crate::Pool;
 use crate::repository::optimism;
 
 #[get("/optimism/batch/tx/latest")]
-pub async fn get_latest_batch_summary(pool: web::Data<Pool>) -> Result<HttpResponse, ExpectedError> {
+pub async fn get_latest_tx_batch_summary(pool: web::Data<Pool>) -> Result<HttpResponse, ExpectedError> {
     let batch_summary = optimism::tx_batch::find_latest_batch_summary(pool)?;
     Ok(HttpResponse::Ok().json(batch_summary))
 }
 
 #[get("/optimism/batch/tx/index/{index}")]
-pub async fn get_batch_by_index(pool: web::Data<Pool>, path_params: web::Path<i64>) -> Result<HttpResponse, ExpectedError> {
+pub async fn get_tx_batch_by_index(pool: web::Data<Pool>, path_params: web::Path<i64>) -> Result<HttpResponse, ExpectedError> {
     let index = path_params.into_inner();
     let batch = optimism::tx_batch::find_batch_by_index(pool, index)?;
     Ok(HttpResponse::Ok().json(batch))
 }
 
 #[get("/optimism/batch/tx/page/{page}/count/{count}")]
-pub async fn get_paginated_batch(pool: web::Data<Pool>, path_params: web::Path<(i64, i64)>) -> Result<HttpResponse, ExpectedError> {
+pub async fn get_paginated_tx_batch(pool: web::Data<Pool>, path_params: web::Path<(i64, i64)>) -> Result<HttpResponse, ExpectedError> {
     let (page, count) = path_params.into_inner();
     let paginated_batch = optimism::tx_batch::find_batch_by_page_count(pool, page, count)?;
     Ok(HttpResponse::Ok().json(paginated_batch))
@@ -42,6 +42,20 @@ pub async fn get_paginated_tx(pool: web::Data<Pool>, path_params: web::Path<(i64
     let (index, page, count) = path_params.into_inner();
     let paginated_tx = optimism::tx::find_tx_by_index_page_count(pool, index, page, count)?;
     Ok(HttpResponse::Ok().json(paginated_tx))
+}
+
+#[get("/optimism/batch/stateroot/page/{page}/count/{count}")]
+pub async fn get_paginated_state_batch(pool: web::Data<Pool>, path_params: web::Path<(i64, i64)>) -> Result<HttpResponse, ExpectedError> {
+    let (page, count) = path_params.into_inner();
+    let paginated_batch = optimism::state_batch::find_batch_by_page_count(pool, page, count)?;
+    Ok(HttpResponse::Ok().json(paginated_batch))
+}
+
+#[get("/optimism/batch/stateroot/index/{index}")]
+pub async fn get_state_batch_by_index(pool: web::Data<Pool>, path_params: web::Path<i64>) -> Result<HttpResponse, ExpectedError> {
+    let index = path_params.into_inner();
+    let batch = optimism::state_batch::find_batch_by_index(pool, index)?;
+    Ok(HttpResponse::Ok().json(batch))
 }
 
 #[get("/optimism/tx/l1tol2/latest")]
