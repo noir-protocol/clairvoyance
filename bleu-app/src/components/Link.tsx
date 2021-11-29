18d2b64d3122c42807005e3be0376155a99a07d5
Jeeyong Um <conr2d@gmail.com>
2021-11-29 17:01:54 +0900

    Add tooltip for text with ellipsis

--- bleu-app/src/components/Link.tsx
+++ bleu-app/src/components/Link.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import {
   Link,
+  Tooltip,
 } from '@mui/material';
 import {l1Explorer} from '../utils/urlResolver';
 
 export function L1AddressLink(props: any) {
   return (
-    <Link variant='mono' underline='none' noWrap={true} href={l1Explorer('/address/', props.address)} sx={props.sx}>
-      {props.address}
-    </Link>
+    <Tooltip title={props.address} disableInteractive>
+      <Link variant='mono' underline='none' noWrap={true} href={l1Explorer('/address/', props.address)} sx={props.sx}>
+        {props.address}
+      </Link>
+    </Tooltip>
   );
 }
 
 export function L2AddressLink(props: any) {
   return (
-    <Link variant='mono' underline='none' noWrap={true} href={`/account/${props.address}`} sx={props.sx}>
-      {props.address}
-    </Link>
+    <Tooltip title={props.address} disableInteractive>
+      <Link variant='mono' underline='none' noWrap={true} href={`/account/${props.address}`} sx={props.sx}>
+        {props.address}
+      </Link>
+    </Tooltip>
   );
 }
 
 export function L1TransactionLink(props: any) {
   return (
-    <Link variant='mono' underline='none' noWrap={true} href={l1Explorer('/tx', props.hash)} target='_blank' rel='noreferrer' sx={props.sx}>
-      {props.hash}
-    </Link>
+    <Tooltip title={props.hash} disableInteractive>
+      <Link variant='mono' underline='none' noWrap={true} href={l1Explorer('/tx', props.hash)} target='_blank' rel='noreferrer' sx={props.sx}>
+        {props.hash}
+      </Link>
+    </Tooltip>
   );
 }
 
 export function L2TransactionLink(props: any) {
   return (
-    <Link variant='mono' underline='none' noWrap={true} href={`/tx/${props.hash}`} sx={props.sx}>
-      {props.hash}
-    </Link>
+    <Tooltip title={props.hash} disableInteractive>
+      <Link variant='mono' underline='none' noWrap={true} href={`/tx/${props.hash}`} sx={props.sx}>
+        {props.hash}
+      </Link>
+    </Tooltip>
   );
 }
 
